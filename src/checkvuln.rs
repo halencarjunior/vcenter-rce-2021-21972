05078c3a17fb4a2a62fd26f1aee04b75fdc0a6e7
Humberto Jr <halencarjunior@gmail.com>
2021-02-26 19:34:04 -0300

    Version 0.2

--- src/checkvuln.rs
+++ src/checkvuln.rs
@@ -2,19 +2,23 @@ use reqwest::ClientBuilder;
 use reqwest::Error;
 use reqwest::header;
 use colorful::{Color, Colorful};
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::time::Duration;
 
 #[tokio::main]
 pub async fn checkvuln(host: String) -> Result<(), Error> {
+    //let timeout = Duration::new(2,0);
 
     let mut headers = header::HeaderMap::new();
     headers.insert("Content-Length", header::HeaderValue::from_static("0"));
 
     let url1 = format!("https://{}/ui/vropspluginui/rest/services/getstatus", host);
     let url2 = format!("https://{}/ui/vropspluginui/rest/services/uploadova", host);
-    let url3 = format!("https://{}/ui/vropspluginui/rest/services/uploadova", host);
   
     println!("{}", "VMware vCenter CVE-2021-21972 checker".color(Color::Yellow).bold());
-    println!("{}", "v.0.1 by bt0".color(Color::Yellow).bold());
+    println!("{}", "v.0.2 by bt0".color(Color::Yellow).bold());
     println!("{}", "=====================================".color(Color::Yellow).bold());
     println!("{}", "Disclaimer: Do not use it on hosts that you don't own!".color(Color::Red).bold());
     println!("{}", "Just for Educational Purposes".color(Color::Red).bold());
@@ -22,59 +26,66 @@ pub async fn checkvuln(host: String) -> Result<(), Error> {
    
     
     // Testing 1st URL using get method
-    println!("[+] Testing url 1: {}",url1);
-    let client = ClientBuilder::new().danger_accept_invalid_certs(true).build()?;
+    println!("[+] Checking url: {}",url1);
+    let client = ClientBuilder::new().danger_accept_invalid_certs(true).timeout(Duration::from_secs(2)).build()?;
     let response = client.get(&url1).send().await?;
 
     if response.status().is_success() {
-        println!("{}", "[-] Host Not Vulnerable\n".color(Color::Green));
-    } else if response.status().is_server_error() {
-        let body = response.text().await?;
-        if body.contains("uploadFile") {
-            println!("{}", "[+] Host is Vulnerable\n".color(Color::Red).bold());
-        } else {
-        println!("{}", "[-] Host Not Vulnerable\n".color(Color::Green));
-        }
-    } else {
-        println!("{}", "[-] Host Not Vulnerable\n".color(Color::Green));
-    }
-
-    // Testing 2nd URL using get method
-    println!("[+] Testing url 2: {}",url2);
-    let client = ClientBuilder::new().danger_accept_invalid_certs(true).build()?;
-    let response = client.get(&url2).send().await?;
-
-    if response.status().is_success() {
-        println!("{}", "[-] Host Not Vulnerable\n".color(Color::Green));
-    } else if response.status().is_server_error() {
-        let body = response.text().await?;
-        if body.contains("uploadFile") {
-            println!("{}", "[+] Host is Vulnerable\n".color(Color::Red).bold());
-        } else {
-        println!("{}", "[-] Host Not Vulnerable\n".color(Color::Green));
-        }
-    } else {
-        println!("{}", "[-] Host Not Vulnerable\n".color(Color::Green));
-    }
-
-    // Testing 3rd URL using post method
+        println!("{}", " ==> [+] Host is accepting requests without any auth. Will Test for POST\n".color(Color::Red));
+        
+        /* Testing URL2 with POST method */
+        // Testing 2nd URL using post method
     
-    println!("[+] Testing url 3: {}",url3);
-    let client = ClientBuilder::new().danger_accept_invalid_certs(true).default_headers(headers).build()?;
-    let response = client.post(&url3).header("Content-Lenght", "0").send().await?;
-    
-    if response.status().is_success() {
-        println!("{}", "[-] Host Not Vulnerable\n".color(Color::Green));
+        println!("[+] Testing url for Vuln: {}",url2);
+        let client = ClientBuilder::new().danger_accept_invalid_certs(true).default_headers(headers).timeout(Duration::from_secs(2)).build()?;
+        let response = client.post(&url2).header("Content-Lenght", "0").send().await?;
+        
+        if response.status().is_success() {
+            println!("{}", " [-] Host Not Vulnerable\n".color(Color::Green));
+        } else if response.status().is_server_error() {
+            let body = response.text().await?;
+            if body.contains("uploadFile") {
+                println!("{}", " [+] Host is Vulnerable\n".color(Color::Red).bold());
+                println!("Workarounds: \nhttps://kb.vmware.com/s/article/82374\nhttps://kb.vmware.com/s/article/76372\n")
+                
+            } else {
+            println!("{}", " [-] Host Not Vulnerable\n".color(Color::Green));
+            }
+        } else {
+            println!("{}", " [-] Host Not Vulnerable\n".color(Color::Green));
+        }
     } else if response.status().is_server_error() {
         let body = response.text().await?;
         if body.contains("uploadFile") {
-            println!("{}", "[+] Host is Vulnerable\n".color(Color::Red).bold());
+            println!("{}", "  [+] Host is Vulnerable\n".color(Color::Red).bold());
         } else {
-        println!("{}", "[-] Host Not Vulnerable\n".color(Color::Green));
+        println!("{}", "  [-] Host Not Vulnerable\n".color(Color::Green));
         }
     } else {
-        println!("{}", "[-] Host Not Vulnerable\n".color(Color::Green));
+        println!("{}", "  [-] Host Not Vulnerable\n".color(Color::Green));
     }
 
     Ok(())
+}
+
+pub fn file_list(filename: String) {
+
+    if let Ok(lines) = read_lines(filename) {
+        for line in lines {
+            if let Ok(ip) = line {
+                checkvuln(ip);
+                //println!("IP: {}", ip);
+            }
+        }
+    }
+
+    //let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    //println!("Text:\n{}", contents);
+}
+
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 mod checkvuln;
-use checkvuln::{checkvuln};
-use std::env;
+use checkvuln::checkvuln;
+use checkvuln::file_list;
 use clap::{Arg, App};
-
 
 fn main() {
 
     let matches = App::new("VMware vCenter CVE-2021-21972 checker")
-                    .version("0.1")
+                    .version("0.2")
                     .author("bt0 bt0@tutanota.com")
                     .about("Check for CVE-2021-21972")
                     .arg(Arg::with_name("host")
@@ -15,23 +14,24 @@ fn main() {
                             .long("host")
                             .value_name("Host IP or domain")
                             .help("Host IP or Domain to be checked for CVE")
-                            .takes_value(true)
-                            .required(true))
-                    .arg(Arg::with_name("ID")
-                            .short("i")
-                            .long("test-id")
-                            .value_name("ID [1-3]")
-                            .help("Chose the URL to test by number 1-3")
                             .takes_value(true))
-                    .arg(Arg::with_name("all")
-                            .short("a")
-                            .long("all")
-                            .help("Perform all tests on all URLs")
+                            //.required(true))
+                    .arg(Arg::with_name("file")
+                            .short("f")
+                            .long("file")
+                            .help("File containing a list of IPs")
                             .takes_value(true))
                     .get_matches();
 
-    let ip = matches.value_of("host").unwrap();
+    if matches.is_present("host") {
+        let ip = matches.value_of("host").unwrap();
+        checkvuln(String::from(ip));
+    } else if matches.is_present("file"){
+        println!("Using file with ip list");
+        let filename = matches.value_of("file").unwrap();
+        file_list(filename.to_string());
+    };    
 
-    checkvuln(ip.to_string());    
+        
     
 }
